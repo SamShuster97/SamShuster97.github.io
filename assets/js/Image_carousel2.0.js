b7ee538ed95e4ee3f0e2f8ee2d2390226e6217fa
Sam Shuster <s.shuster97@gmail.com>
2024-02-16 11:14:43 -0500

    Continued modifications.

---
+++ assets/js/Image_carousel2.0.js
@@ -0,0 +1,14 @@
+const items = document.querySelectorAll('.item');
+
+const reset = () => items.forEach((item) => item.classList.remove('animation'));
+
+function accordion(e) {
+  if (!e.target.closest('.item')) return;
+  reset();
+  e.target.parentElement.classList.add('animation');
+}
+
+const init = () => items[items.length-1].classList.add('animation');
+
+window.addEventListener('load',init,false);
+window.addEventListener('click',accordion,false);
